011f6f47ffe353b2e7a788f75ffaab59299629c7
Reinald Reynoso <reireynoso@gmail.com>
2019-03-08 20:07:05 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
