721246b178f15b08a533971fe1f94f1659798cfa
Reinald Reynoso <reireynoso@gmail.com>
2019-03-08 20:06:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,12 @@
 
 def countdown(integer)
   while integer > 0
-  
     puts "#{integer} SECOND(S)!"
     integer -= 1 
   end
   return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+  sleep(num)
 end
