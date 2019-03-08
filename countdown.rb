7ba77497842aaf4716dd387cb0518d07cb9046dc
Reinald Reynoso <reireynoso@gmail.com>
2019-03-08 19:52:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  counter = 0
+  while integer > counter 
+    puts "#{integer} SECOND(S)!"
+    integer -= 1 
+  end
+  puts "HAPPY NEW YEAR!"
 end
